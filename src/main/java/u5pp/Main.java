41a7d986145eccd199d3ab3fdebc4162ae5a97d4
Eric Hsieh <eric.kf.hsieh@gmail.com>
2021-10-19 16:12:45 -0700

    Add default print out functionality to Main to lesson confusion

--- src/main/java/u5pp/Main.java
+++ src/main/java/u5pp/Main.java
@@ -2,5 +2,6 @@ package u5pp;
 
 class Main {
   public static void main(String[] args) {  
+    System.out.println("Hi! If you're seeing this, then you just ran your main method.... Nothing is here though :)");
   }
 }
